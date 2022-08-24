dffc41d806403a031444de9053c2589dcebeb9da
ignacio <iaheck@uc.cl>
2022-08-24 18:24:33 -0300

    feat(path): simplifies migrations

--- db/migrate/20220824195622_create_paths.rb
+++ db/migrate/20220824195622_create_paths.rb
@@ -1,7 +1,7 @@
 class CreatePaths < ActiveRecord::Migration[7.0]
   def change
     create_table :paths do |t|
-      t.string :path_name
+      t.string :name
       t.text :description
 
       t.timestamps
--- db/migrate/20220824200954_rename_path_name_to_name.rb
+++
@@ -1,5 +0,0 @@
-class RenamePathNameToName < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :name, :description
-  end
-end
