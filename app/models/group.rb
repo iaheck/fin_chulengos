0f5c2411cab88ff48934adec01b17d21cf042630
Carlos Riquelme <criquelme2@gmail.com>
2022-08-25 17:14:10 -0300

    feat(groups, users): Create relations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
     has_many :GroupChallengeAssignment, dependent: :destroy
+    has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable
   has_many :UserChallengeAssignment, dependent: :destroy
+  has_and_belongs_to_many :groups
 end
