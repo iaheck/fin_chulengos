73e999693421a92106166ca00574bb00bd37a467
ignacio <iaheck@uc.cl>
2022-08-25 17:22:28 -0300

    feat(pathDrillDown): path-user relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   has_and_belongs_to_many :groups
   has_many :UserReadResource, dependant: :destroy
   has_many :Resource, :through => :UserReadResource, dependant: :destroy
+
+  has_many :UserPath, dependant: :destroy
+  has_many :Path, :through => :UserPath
+
+  has_many :UserMentorPath, dependant: :destroy
+  has_many :Path, :through => :UserMentorPath
 end
--- app/models/user_mentor_path.rb
+++ app/models/user_mentor_path.rb
@@ -1,2 +1,4 @@
 class UserMentorPath < ApplicationRecord
+    belongs_to :User
+    belongs_to :Path
 end
