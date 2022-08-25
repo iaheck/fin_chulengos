4b630f870a42b4803a2632e1d18b0c15a4cf17eb
ignacio <iaheck@uc.cl>
2022-08-25 17:06:20 -0300

    feat(pathDrillDown): path-checkpoint relations

--- app/models/path.rb
+++ app/models/path.rb
@@ -1,2 +1,3 @@
 class Path < ApplicationRecord
+    has_and_belongs_to_many :Checkpoint
 end
--- db/migrate/20220824213356_create_path_checkpoints.rb
+++ db/migrate/20220824213356_create_path_checkpoints.rb
@@ -1,6 +1,6 @@
 class CreatePathCheckpoints < ActiveRecord::Migration[7.0]
   def change
-    create_table :path_checkpoints do |t|
+    create_table :path_checkpoints, id: false do |t|
       t.integer :path_id
       t.integer :checkpoint_id
 
