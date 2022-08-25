6513f9a6d23be8d7ea8972b37ea051a3befef835
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-08-25 10:20:34 -0300

    feat(db-challenge): ReviewSubmitPersonalChallenge db creation

---
+++ app/models/review_submit_personal_challenge.rb
@@ -0,0 +1,2 @@
+class ReviewSubmitPersonalChallenge < ApplicationRecord
+end
---
+++ db/migrate/20220825132024_create_review_submit_personal_challenges.rb
@@ -0,0 +1,12 @@
+class CreateReviewSubmitPersonalChallenges < ActiveRecord::Migration[7.0]
+  def change
+    create_table :review_submit_personal_challenges do |t|
+      t.integer :submit_personal_challenge_id
+      t.text :comment
+      t.datetime :reviewed_at
+      t.datetime :approved_at
+
+      t.timestamps
+    end
+  end
+end
