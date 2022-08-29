b604101336bdc60ed496e8bc344e6468724ed390
ignacio <iaheck@uc.cl>
2022-08-29 12:53:48 -0300

    feat(checkpoints): new checkpoint create controller

--- app/controllers/checkpoints_controller.rb
+++ app/controllers/checkpoints_controller.rb
@@ -1,8 +1,27 @@
 class CheckpointsController < ApplicationController
   def index
   end
-  
+
   def show
     @checkpoint = Checkpoint.find(params[:id])
   end
+
+  def new
+    @checkpoint = Checkpoint.new
+  end
+
+  def create
+    @checkpoint = Checkpoint.new(checkpoint_params)
+
+    if @checkpoint.save
+      redirect_to @checkpoint
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def checkpoint_params
+      params.require(:checkpoint).permit(:name, :description)
+    end
 end
