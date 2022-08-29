2b92e04ca3f93dcd112a9ab628e200bda3db0c40
ignacio <iaheck@uc.cl>
2022-08-29 11:48:29 -0300

    fix(dependant):changes to dependent

--- app/models/path.rb
+++ app/models/path.rb
@@ -1,9 +1,9 @@
 class Path < ApplicationRecord
     has_and_belongs_to_many :Checkpoint
 
-    has_many :UserPath, dependant: :destroy
+    has_many :UserPath, dependent: :destroy
     has_many :User, :through => :UserPath
 
-    has_many :UserMentorPath, dependant: :destroy
+    has_many :UserMentorPath, dependent: :destroy
     has_many :User, :through => :UserMentorPath
 end
