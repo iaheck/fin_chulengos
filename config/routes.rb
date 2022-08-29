8a25d784cc63a538429397df0061a70d269a0f42
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-08-29 15:58:03 -0300

    feat(feat-login): change name from home to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'login/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :resource
-  # root to: "home#index"
+
+  root to: "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
   resources :paths, only: [:show] do
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   resources :checkpoints, only: [:show]
 
   root "paths#show", :defaults => { :id => '1' }
+  #root to:"login#index"
 end
