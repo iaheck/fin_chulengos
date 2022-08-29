51a3b8e23f3a35c1566fb6b5fa750f003c84eb04
Carlos Riquelme <criquelme2@gmail.com>
2022-08-29 11:23:32 -0300

    feat(resources): fix typo

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,5 @@
 class Resource < ApplicationRecord
     has_and_belongs_to_many :Checkpoint
-
-    has_many :UserReadResource, dependant: :destroy
-    has_many :Users, :through => :UserReadResource, dependant: :destroy
+    has_many :UserReadResource, dependent: :destroy
+    has_many :Users, :through => :UserReadResource, dependent: :destroy
 end
