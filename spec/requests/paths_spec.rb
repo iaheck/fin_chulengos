8fa6eba089f8f420442e31f17867b50491b2621a
ignacio <iaheck@uc.cl>
2022-08-30 14:26:02 -0300

    feat(path): test show path

--- spec/requests/paths_spec.rb
+++ spec/requests/paths_spec.rb
@@ -10,35 +10,11 @@ RSpec.describe PathsController, type: :request do
     end
   end
 
-  # describe "POST /create" do
-  #   let(:name) { 'Pedro' }
-
-  #   def execute
-  #     post resource_index_path, params: {
-  #       name: name,
-  #       description: 'Contenido',
-  #       url: 'http://google.cl'
-  #     }
-  #   end
-
-  #   it do
-  #     expect { execute }.to change { Resource.count }.by(1)
-  #     expect(Resource.last).to have_attributes(
-  #       name: 'Pedro',
-  #       description: 'Contenido',
-  #       url: 'http://google.cl'
-  #     )
-  #   end
-
-  #   context "with missing name param" do
-  #     let(:name) { nil }
-
-  #     it "doesn't create a new record" do
-  #       expect do
-  #         execute
-  #       rescue StandardError => e
-  #       end.to change { Resource.count }.by(0)
-  #     end
-  #   end
+  describe "show path name" do
+    it "The page must have the path name" do
+      get path_path(path.id)
+      expect(response.body).to include("sendero")
+    end
+  end
 
 end
