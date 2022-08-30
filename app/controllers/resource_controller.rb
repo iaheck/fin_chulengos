55e130730980cdcc821c6b79fd94d2a0d44b0d0e
Carlos Riquelme <criquelme2@gmail.com>
2022-08-30 10:30:00 -0300

    feat(resources): update controller

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -1,11 +1,11 @@
 class ResourceController < ApplicationController
+    before_action :set_resource, only: [:show]
+
     def index
         @list_resources = Resource.all
     end
     
-    def show
-        @resource = Resource.find(params[:id])
-    end
+    def show; end
 
     def new
         @resource = Resource.new
@@ -19,6 +19,11 @@ class ResourceController < ApplicationController
         else
           render :new, status: :unprocessable_entity
         end
+    end
+
+    private
+    def set_resource
+        @resource = Resource.find(params[:id])
     end
 
 end
