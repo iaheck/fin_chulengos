3a5814dd7707178eef3fcb74e84065772a5636b0
ignacio <iaheck@uc.cl>
2022-08-31 12:03:43 -0300

    feat(checkpoint): adds create tests

--- spec/requests/checkpoints_spec.rb
+++ spec/requests/checkpoints_spec.rb
@@ -8,4 +8,35 @@ RSpec.describe "Checkpoints", type: :request do
     # end
   end
 
+  describe "POST /create" do
+    let(:name) { 'github' }
+    let(:description) { 'this is a nice checkpoint' }
+
+    def execute
+      post resource_index_path, params: {
+        name: name,
+        description: description
+      }
+    end
+
+    it do
+      expect { execute }.to change { Resource.count }.by(1)
+      expect(Resource.last).to have_attributes(
+        name: name,
+        description: description
+      )
+    end
+
+    context "with missing name param" do
+      let(:name) { nil }
+
+      it "doesn't create a new record" do
+        expect do
+          execute
+        rescue StandardError => e
+        end.to change { Resource.count }.by(0)
+      end
+    end
+  end
+
 end
