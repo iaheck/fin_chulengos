b69644ca8bc9ea0d3cafd998ed745a56d2385a70
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-09-08 11:55:09 -0300

    feat: Just show Roadmap when signed in

--- app/controllers/roadmaps_controller.rb
+++ app/controllers/roadmaps_controller.rb
@@ -1,5 +1,8 @@
 class RoadmapsController < ApplicationController
   def show
+    if !user_signed_in? 
+      redirect_to '/login/index'
+    end
     @roadmap = Roadmap.find(params[:id])
   end
 end
