0e68ebf47ca3f4040152f0a8dfdecb959666a17c
Carlos Riquelme <criquelme2@gmail.com>
2022-09-08 11:45:58 -0300

    feat(checkpointsResources): Create

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,11 +1,11 @@
 class ResourcesController < ApplicationController
   before_action :set_resource, only: [:show]
-  before_action :set_checkpoint, only: [:index]
+  before_action :set_checkpoint, only: [:create, :index]
 
   def index
     @list_resources = Resource.all
   end
-
+  
   def show; end
 
   def new
@@ -13,22 +13,53 @@ class ResourcesController < ApplicationController
   end
 
   def create
-    @resource = Resource.new(name: params[:name], description: params[:description], url: params[:url])
-
+    @resource = Resource.new(
+      name: get_resource_name,
+      description: get_resource_description,
+      url: get_resource_url
+    )
+    
     if @resource.save
-      redirect_to @resource
+      # @checkpoint = Checkpoint.find(params[:checkpoint_id])
+      redirect_to checkpoint_resource_path(@checkpoint), flash: { success: "Recurso creado con éxito" }
     else
-      render :index, status: :unprocessable_entity
+      redirect_to checkpoint_resource_path(@checkpoint), flash: { error: "Error al crear el recurso" }
+      # render :index, status: :unprocessable_entity
     end
   end
 
   private
-
   def set_resource
     @resource = Resource.find(params[:id])
   end
+
   def set_checkpoint
+    if params[:resource].present?
+      checkpoint_id = params[:resource][:checkpoint_id]
+    else
+      checkpoint_id = params[:id]
+    end
+
+    @checkpoint = Checkpoint.find(checkpoint_id)
+  end
+
+  def set_checkpoint_from_id
     @checkpoint = Checkpoint.find(params[:id])
   end
 
+  def set_checkpoint_from_checkpoint_id
+    @checkpoint = Checkpoint.find(params[:resource][:checkpoint_id])
+  end
+
+  def get_resource_name
+    params[:resource][:name]
+  end
+
+  def get_resource_description
+    params[:resource][:description]
+  end
+
+  def get_resource_url
+    params[:resource][:url]
+  end
 end
