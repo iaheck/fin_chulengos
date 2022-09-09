5815ca9eb7c5bff90a247f2a8dfb0162420f90a6
Carlos Riquelme <criquelme2@gmail.com>
2022-09-09 12:05:19 -0300

    feat(rootStyle): fix spec and route

--- spec/requests/login_spec.rb
+++ spec/requests/login_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Logins', type: :request do
+  let!(:user) { create(:user) }
+  before do
+    sign_in user
+  end
   describe 'GET /index' do
     it 'returns http success' do
       get '/login/index'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(root_path)
     end
   end
 end
