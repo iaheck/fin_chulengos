a570c955300ca63b1faa0591c40940446b07e32c
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-09-09 16:22:30 -0300

    feat: Link for user enrollment and state change

--- app/controllers/challenge_personals_controller.rb
+++ app/controllers/challenge_personals_controller.rb
@@ -6,12 +6,7 @@ class ChallengePersonalsController < ApplicationController
     @list_challenge_personal = @checkpoint.challenge_personals
   end
 
-  def show
-    @user_already_enrolled = UserChallengeAssigment.exists?(
-      user_id: current_user.id,
-      challenge_personal_id: @challenge_personal.id
-    )
-  end
+  def show; end
 
   def new
     @challenge_personal = ChallengePersonal.new
@@ -31,20 +26,30 @@ class ChallengePersonalsController < ApplicationController
   end
 
   def enroll_challenge
-    @user_challenge_assignment = UserChallengeAssigment.new(user_id: current_user.id,
-                                                            challenge_personal_id: @challenge_personal.id)
-    if @user_challenge_assignment.save
-      flash[:notice] = 'Estás inscrito en el Desafío!'
+    if !@user_already_enrolled
+      @user_challenge_assignment = UserChallengeAssigment.new(user_id: current_user.id,
+                                                              challenge_personal_id: @challenge_personal.id)
+      if @user_challenge_assignment.save
+        flash[:notice] = 'Estás inscrito en el Desafío!'
+        puts 'Estás inscrito en el Desafío!'
+      else
+        flash[:alert] = 'Error al inscribirse en el Desafío'
+        puts 'Error al inscribirse en el Desafío'
+      end
     else
-      flash[:alert] = 'Error al inscribirse en el Desafío'
+      flash[:alert] = 'Ya estas inscrito en el Desafío'
+      puts 'Ya estas inscrito en el Desafío'
     end
-    redirect_to @challenge_personal
+    redirect_to checkpoint_challenge_personal_path
   end
 
   private
 
   def set_challenge_personal
     @challenge_personal = ChallengePersonal.find(params[:id])
+    @user_already_enrolled = UserChallengeAssigment.exists?(
+      user_id: current_user.id,
+      challenge_personal_id: @challenge_personal.id)
   end
 
   def set_checkpoint
