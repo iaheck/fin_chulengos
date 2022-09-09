6deb4e6a5226c445452d4d04ff4e71eafbf7e626
ignacio <iaheck@uc.cl>
2022-09-09 12:48:58 -0300

    feat(scopeHammering): rubocop schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,87 +12,86 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_09_154227) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "challenge_personals", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "url"
-    t.integer "checkpoint_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'challenge_personals', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.string 'url'
+    t.integer 'checkpoint_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "checkpoint_resources", force: :cascade do |t|
-    t.integer "checkpoint_id"
-    t.integer "resource_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'checkpoint_resources', force: :cascade do |t|
+    t.integer 'checkpoint_id'
+    t.integer 'resource_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "checkpoint_roadmaps", force: :cascade do |t|
-    t.integer "roadmap_id"
-    t.integer "checkpoint_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'checkpoint_roadmaps', force: :cascade do |t|
+    t.integer 'roadmap_id'
+    t.integer 'checkpoint_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "checkpoints", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'checkpoints', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "resources", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'resources', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.string 'url'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "roadmap_users", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "roadmap_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'roadmap_users', force: :cascade do |t|
+    t.integer 'user_id'
+    t.integer 'roadmap_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "roadmaps", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'roadmaps', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "user_challenge_assigments", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "challenge_personal_id"
-    t.datetime "started_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'user_challenge_assigments', force: :cascade do |t|
+    t.integer 'user_id'
+    t.integer 'challenge_personal_id'
+    t.datetime 'started_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "full_name"
-    t.string "github_user"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.integer 'failed_attempts', default: 0, null: false
+    t.string 'unlock_token'
+    t.datetime 'locked_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'full_name'
+    t.string 'github_user'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
