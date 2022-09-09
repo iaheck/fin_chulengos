9837108abc55f731341adcfa29e068d7be57b626
Carlos Riquelme <criquelme2@gmail.com>
2022-09-09 12:09:46 -0300

    feat(rootStyle): fix lint

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,6 @@
 class LoginController < ApplicationController
   def index
     redirect_to root_path if user_signed_in?
-    redirect_to new_user_session_path if !user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
