0cc8f0558a937d330777313e18903c0a378cea09
Vanessa Pulgar <vanessa.pulgardiez@gmail.com>
2022-09-09 12:18:49 -0300

    feat: Rucocop fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,7 @@ module Users
     before_action :configure_sign_up_params, only: [:create]
     before_action :configure_account_update_params, only: [:update]
 
-    # POST /resource
+    #  POST /resource
     def create
       super
 
